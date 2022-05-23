a5e2bf965b3503a1d0b8682d0b9a4786a7d52b88
A90G <104040612+A90G@users.noreply.github.com>
2022-05-23 15:03:20 -0300

    commit can´t make it work... possible scoop trouble

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import "./styles.css";
+/*• Leer valores hasta que se introduzca un cero (0)
+• El usuario puede introducir valores positivos y negativos
+• Encontrar el máximo de los elementos que se introdujeron
+• Analizar cómo cambia el programa para hallar el mínimo
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+
+Ingrese número: 9
+Ingrese número: 7
+Ingrese número: -1
+Ingrese número: 1
+Ingrese número: 0
+El máximo es 9
+*/
+let ingresos: boolean = false;
+let num: number = Number(prompt("Ingrese un número que desee"));
+let numMax: number = num;
+
+while (num != 0) {
+  ingresos = true;
+
+  if (num > numMax) {
+    numMax = num;
+
+    let num = Number(prompt("Ingrese otro número que desee"));
+  }
+}
+//console.log(`Se ingreso ${menor} como número menor`);
+console.log(`Se ingreso ${numMax} como número mayor`);
+//tengo un problema de scoops ;(
